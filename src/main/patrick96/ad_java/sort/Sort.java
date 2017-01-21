a8c92d3cb1d20cc83475d0b52d9466de77367e7c
patrick96 <p.ziegler96@gmail.com>
2017-01-21 22:10:08 +0100

    Improve bubbleSort average case

--- src/main/patrick96/ad_java/sort/Sort.java
+++ src/main/patrick96/ad_java/sort/Sort.java
@@ -26,10 +26,24 @@ public class Sort {
     public static int[] bubbleSort(int[] array) {
         int n = array.length;
         for (int i = 0; i < n; i++) {
+            // Flag to store, whether or not at least one element was swapped
+            boolean didSwap = false;
             for (int j = 1; j < n - i; j++) {
                 if(array[j - 1] > array[j]) {
+                    didSwap = true;
                     Utils.swap(array, j, j - 1);
                 }
+            }
+
+            if(!didSwap) {
+                /*
+                 * If no element was swapped during this iteration, then the
+                 * array is now sorted, meaning we can finish early
+                 *
+                 * This does not impact the worst case but lowers the best
+                 * case key comparisons to (n - 1)
+                 */
+                break;
             }
         }
 
